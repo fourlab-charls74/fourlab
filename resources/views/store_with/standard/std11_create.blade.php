388d3f141c6ab4004662bd56e47c6650c239f9f0
madforre <madforre@gmail.com>
2022-06-23 12:27:14 +0900

    style(std11_create): 수선관리 추가 페이지 퍼블리싱

--- resources/views/store_with/standard/std11_create.blade.php
+++ resources/views/store_with/standard/std11_create.blade.php
@@ -3,6 +3,18 @@
 @section('content')
 
 <style>
+
+    .search_cum_form .row {
+        margin-top: 20px;
+    }
+
+    .card, #search-area .card-header {
+        background: #eeeef0 !important;
+    }
+
+    .card {
+        border: 1px solid #ddd;
+    }
     .required::after {
         position: relative;
         top: 2px;
@@ -22,10 +34,6 @@
         <div class="card mb-3">
             <div class="d-flex card-header justify-content-between">
                 <h4>입력</h4>
-                <div>
-                    <a href="/store/standard/std11/" class="btn btn-sm btn-primary shadow-sm pl-2"><i class="bx bx-list-ul mr-1"></i>목록</a>
-                    <a href="/store/standard/std11/create" class="btn btn-sm btn-primary shadow-sm pl-2"><i class="bx bx-save mr-1"></i>저장</a>
-                </div>
             </div>
             <div class="card-body">
                 <div class="row">
@@ -222,83 +230,148 @@
                         </div>
                     </div>
                 </div>
-                <div class="search-area-ext d-none row">
+                <div class="row">
                     <div class="col-lg-4 inner-td">
                         <div class="form-group">
-                            <label for="item">카테고리</label>
-                            <div class="form-inline inline_select_box">
-                                <div class="form-inline-inner select-box">
-                                    <select name='cat_type' class="form-control form-control-sm">
-                                        <option value='DISPLAY'>전시</option>
-                                        <option value='ITEM'>용도</option>
-                                    </select>
-                                </div>
-                                <div class="form-inline-inner input-box">
-                                    <div class="form-inline inline_btn_box">
-                                        <input type='hidden' name='cat_cd' id='cat_cd' value=''>
-                                        <input type='text' class="form-control form-control-sm" name='cat_nm' id='cat_nm' value=''>
-                                        <a href="#" class="btn btn-sm btn-outline-primary sch-category"><i class="bx bx-dots-horizontal-rounded fs-16"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 inner-td">
-                        <div class="form-group">
-                            <label for="item">재고구분</label>
+                            <label for="use_y" class="required">수선유료구분</label>
                             <div class="form-inline form-radio-box">
                                 <div class="custom-control custom-radio">
-                                    <input type="radio" name="is_unlimited" id="is_unlimited1" class="custom-control-input" checked="" value="N">
-                                    <label class="custom-control-label" for="is_unlimited1" value="20">수량관리함</label>
+                                    <input type="radio" name="is_free" id="use_y" class="custom-control-input" value="Y"/>
+                                    <label class="custom-control-label" for="use_y">유료</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                    <input type="radio" name="is_unlimited" id="is_unlimited2" class="custom-control-input" value="Y">
-                                    <label class="custom-control-label" for="is_unlimited2" value="30">수량 관리 안함(무한재고)</label>
+                                    <input type="radio" name="is_free" id="use_n" class="custom-control-input" value="N" checked/>
+                                    <label class="custom-control-label" for="use_n">무료</label>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 inner-td">
                         <div class="form-group">
-                            <label for="item">자료수/정렬</label>
+                            <label for="charged_price">수선금액</label>
+                            <div class="form-inline">
+                                <div class="form-inline-inner input_box">
+                                    <input type='text' class="form-control form-control-sm search-enter" name='charged_price' id="charged_price" value="" placeholder="유료비용" onkeyup="onlynum(this)">
+                                </div>
+                                <span class="text_line">/</span>
+                                <div class="form-inline-inner input_box">
+                                    <input type="text" class="form-control form-control-sm search-enter" name="free_price" value="" placeholder="무료비용" onkeyup="onlynum(this)">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 inner-td">
+                        <div class="form-group">
+                            <label for="as_content" class="required">수선내용</label>
                             <div class="flex_box">
-                                <select name="limit" class="form-control form-control-sm">
-                                    <option value="100">100</option>
-                                    <option value="500">500</option>
-                                    <option value="1000">1000</option>
-                                    <option value="2000">2000</option>
-                                </select>
+                                <textarea name="content" id="as_content" class="form-control form-control-sm" style="height: 200px;"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="resul_btn_wrap mb-3">
-            <a href="#" id="search_sbtn" onclick="return Search();" class="btn btn-sm btn-primary shadow-sm pl-2"><i class="fas fa-search fa-sm text-white-50"></i> 조회</a>
-            <a href="/store/standard/create" class="btn btn-sm btn-outline-primary shadow-sm pl-2"><i class="bx bx-plus fs-16"></i> 추가</a>
-            <div class="search_mode_wrap btn-group mr-2 mb-0 mb-sm-0"></div>
+        <div class="card mb-3">
+            <div class="d-flex card-header justify-content-between">
+                <h4>본사처리 내용</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-4 inner-td">
+                        <div class="form-group">
+                            <label for="h_receipt_date">본사접수일</label>
+                            <div class="form-inline">
+                                <div class="docs-datepicker form-inline-inner input_box w-100">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control form-control-sm docs-date" name="h_receipt_date" value="" id="h_receipt_date" autocomplete="off">
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary docs-datepicker-trigger p-0 pl-2 pr-2" disable="">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="docs-datepicker-container"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 inner-td">
+                        <div class="form-group">
+                            <label for="due_date">수선예정일</label>
+                            <div class="form-inline">
+                                <div class="docs-datepicker form-inline-inner input_box w-100">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control form-control-sm docs-date" name="due_date" value="" id="due_date" autocomplete="off">
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary docs-datepicker-trigger p-0 pl-2 pr-2" disable="">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="docs-datepicker-container"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 inner-td">
+                        <div class="form-group">
+                            <label for="start_date">수선인도일</label>
+                            <div class="form-inline">
+                                <div class="docs-datepicker form-inline-inner input_box w-100">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control form-control-sm docs-date" name="start_date" value="" id="start_date" autocomplete="off">
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary docs-datepicker-trigger p-0 pl-2 pr-2" disable="">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="docs-datepicker-container"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 inner-td">
+                        <div class="form-group">
+                            <label for="end_date">수선완료일</label>
+                            <div class="form-inline">
+                                <div class="docs-datepicker form-inline-inner input_box w-100">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control form-control-sm docs-date" name="end_date" value="" id="end_date" autocomplete="off">
+                                        <div class="input-group-append">
+                                            <button type="button" class="btn btn-outline-secondary docs-datepicker-trigger p-0 pl-2 pr-2" disable="">
+                                                <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="docs-datepicker-container"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 inner-td">
+                        <div class="form-group">
+                            <label for="h_explain">본사설명</label>
+                            <div class="flex_box">
+                                <textarea name="h_explain" id="h_explain" class="form-control form-control-sm" style="height: 200px;"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="text-align: center;">
+            <a href="/store/standard/std11/create" class="btn btn-sm btn-primary shadow-sm pl-2"><i class="bx bx-save mr-1"></i>저장하기</a>
+            <a href="/store/standard/std11/" class="btn btn-sm btn-primary shadow-sm pl-2"><i class="bx bx-list-ul mr-1"></i>목록으로 이동</a>
         </div>
     </div>
 </form>
-
-<div id="filter-area" class="card shadow-none mb-0 search_cum_form ty2 last-card">
-    <div class="card-body shadow">
-        <div class="card-title mb-3">
-            <div class="filter_wrap">
-                <div class="fl_box">
-                    <h6 class="m-0 font-weight-bold">총 <span id="gd-total" class="text-primary">0</span> 건</h6>
-                </div>
-                <div class="fr_box">
-
-                </div>
-            </div>
-        </div>
-        <div class="table-responsive">
-            <div id="div-gd" style="height:calc(100vh - 370px);width:100%;" class="ag-theme-balham"></div>
-        </div>
-    </div>
-</div>
 <script language="javascript">
 var columns = [
         // this row shows the row index, doesn't use any data from the row
