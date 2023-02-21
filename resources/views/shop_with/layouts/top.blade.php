59b866867db2e96f974ed29487a44717ea0e1274
steamingc <steamingc@gmail.com>
2023-02-21 14:57:16 +0900

    fix(): shop header UI 수정 및 개선(작업중)

--- resources/views/shop_with/layouts/top.blade.php
+++ resources/views/shop_with/layouts/top.blade.php
@@ -1,13 +1,18 @@
 <header>
     <div class="top_inner_box">
         <div class="d-flex">
-            <div class="logo pc white"><a href="/store" style="background:url('/theme/{{config('shop.theme')}}/images/pc_logo_white.png') no-repeat center center;"></a></div>
+            <div class="logo pc white"><a href="/shop" style="background:url('/theme/{{config('shop.theme')}}/images/pc_logo_white.png') no-repeat center center;"></a></div>
             <ul class="top_setting_btn">
                 <li><a href="#" class="view" data-toggle="fullscreen"></a></li>
                 <li><a href="#" class="side"></a></li>
                 <li><a href="javascrip:;" class="mode"></a></li>
             </ul>
         </div>
+        @if (auth('head')->user()->store_cd !== '' && auth('head')->user()->grade === 'P')
+        <div class="d-flex">
+            <h1 class="shop-title">{{auth('head')->user()->store_nm}} ({{auth('head')->user()->store_cd}})</h1>
+        </div>
+        @endif
         <div class="d-flex">
             <ul class="top_link_btn">
                 <li><a href="javascript:void(0);" class="pos" onclick="return window.open('/shop/pos', '_blank', 'toolbar=no,scrollbars=yes,resizable=yes,status=yes,top=0,left=0,width=1920,height=980');"></a></li>
@@ -26,7 +31,9 @@
                         <div class="dropdown-menu dropdown-menu-right">
                             <a class="dropdown-item" href="/shop/user"><i class="bx bx-user font-size-16 align-middle mr-1"></i> Profile</a>
                             <a class="dropdown-item" href="/shop/user/log"><i class="bx bx-wallet font-size-16 align-middle mr-1"></i> My Log</a>
+                            @if (auth('head')->user()->store_cd !== '' && auth('head')->user()->grade === 'P')
                             <a class="dropdown-item" href="/shop/standard/std02/show/{{ auth('head')->user()->store_cd }}"><i class="bx bx-store font-size-16 align-middle mr-1"></i> My Store</a>
+                            @endif
                             <a class="dropdown-item d-block" href="#"><span class="badge badge-success float-right">11</span><i class="bx bx-wrench font-size-16 align-middle mr-1"></i> Settings</a>
                             <a class="dropdown-item" href="#"><i class="bx bx-lock-open font-size-16 align-middle mr-1"></i> Lock screen</a>
                             <div class="dropdown-divider"></div>
