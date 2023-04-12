de64dc413da8b3cc9094f31579c666b501077580
bigcastle <bigcastle5407@naver.com>
2023-04-12 10:39:19 +0900

    fix(index):메인화면 공지사항 날짜검색 삭제 limit로 10개만 출력

--- app/Http/Controllers/shop/IndexController.php
+++ app/Http/Controllers/shop/IndexController.php
@@ -326,7 +326,7 @@ class IndexController extends Controller
                 left outer join notice_store_detail d on s.ns_cd = d.ns_cd
                 left outer join store a on a.store_cd = d.store_cd
                 left outer join code c on c.code_kind_cd = 'store_type' and c.code_id = a.store_type
-            where s.rt >= '$sdate' and s.rt < date_add( '$edate', interval 1 day) and s.all_store_yn = 'Y' or d.store_cd = '$user_store'
+            where 1=1 and s.all_store_yn = 'Y' or d.store_cd = '$user_store'
             group by s.ns_cd
             order by s.rt desc
             limit 0, 10
