67e785688b331dd1b3f652ede87ebcc1ed4035fc
ceduce <ceduce@gmail.com>
2023-08-18 10:41:01 +0900

    fix(ord21, 22) - 기간변경

--- app/Http/Controllers/head/order/ord21Controller.php
+++ app/Http/Controllers/head/order/ord21Controller.php
@@ -16,7 +16,7 @@ class ord21Controller extends Controller
 	//
 	public function index() {
 		$mutable  = Carbon::now();
-		$sdate    = $mutable->sub(8, 'week')->format('Y-m-d');
+		$sdate    = $mutable->sub(2, 'month')->format('Y-m-d');
 
 
 		$id = Auth('head')->user()->id;
