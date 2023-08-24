f5ec5c4c903d7f2b64faf595cd10923f272b7f85
ceduce <ceduce@gmail.com>
2023-08-24 18:50:08 +0900

    fix(prd02) - 상품코드관리 사이즈명 미 노출

--- app/Http/Controllers/store/product/prd02Controller.php
+++ app/Http/Controllers/store/product/prd02Controller.php
@@ -235,12 +235,12 @@ class prd02Controller extends Controller
 						and s.size_cd = pc.size
 						and use_yn = 'Y'
 				),'') as size
-				, ifnull((
-					select s.size_nm from size s 
-					where s.size_kind_cd = pc.size_kind 
-						and s.size_cd = pc.size
-						and use_yn = 'Y'
-				), '') as size_nm
+				-- , ifnull((	사이즈명 쓰지마시오.
+				--	select s.size_nm from size s 
+				--	where s.size_kind_cd = pc.size_kind 
+				--		and s.size_cd = pc.size
+				--		and use_yn = 'Y'
+				-- ), '') as size_nm
 				, if(g.special_yn <> 'Y', replace(g.img, '$cfg_img_size_real', '$cfg_img_size_list'), (
 							select replace(a.img, '$cfg_img_size_real', '$cfg_img_size_list') as img
 							from goods a where a.goods_no = g.goods_no and a.goods_sub = 0
