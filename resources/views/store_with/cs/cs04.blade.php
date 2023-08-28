b60e6464e0aa5a6ab893c086d596b0269f44353b
ceduce <ceduce@gmail.com>
2023-08-28 15:59:02 +0900

    fix(cs04) - 창고간 이동 창고 권한 오류 수정

--- resources/views/store_with/cs/cs04.blade.php
+++ resources/views/store_with/cs/cs04.blade.php
@@ -19,7 +19,7 @@
 					<a href="#" id="search_sbtn" onclick="Search();" class="btn btn-sm btn-primary shadow-sm mr-1"><i class="fas fa-search fa-sm text-white-50"></i> 조회</a>
                     <!-- 2023-05-25 검색조건 초기화 주석처리 -양대성- -->
                     <!-- <a href="javascript:void(0);" class="btn btn-sm btn-primary shadow-sm pl-2 mr-1" onclick="initSearch()" hidden>검색조건 초기화</a> -->
-                    @if(Auth('head')->user()->logistics_group_yn == 'N')
+                    @if(Auth('head')->user()->logistics_group_yn != 'A')
                     <a href="javascript:void(0);" onclick="openDetailPopup()" class="btn btn-sm btn-outline-primary shadow-sm pl-2 mr-1"><i class="bx bx-plus fs-16"></i> 이동 등록</a>
                     <a href="javascript:void(0);" onclick="openBatchPopup()" class="btn btn-sm btn-outline-primary shadow-sm pl-2 mr-1"><i class="bx bx-plus fs-16"></i> 이동 일괄등록</a>
                     @endif
