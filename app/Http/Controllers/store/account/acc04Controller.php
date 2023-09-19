d0c676e90af686b27cabe0ae307412529e310e8d
ceduce <ceduce@gmail.com>
2023-09-19 14:23:56 +0900

    fix(acc04) - 기타재반 오류 수정

--- app/Http/Controllers/store/account/acc04Controller.php
+++ app/Http/Controllers/store/account/acc04Controller.php
@@ -218,7 +218,7 @@ class acc04Controller extends Controller
                 ) a
             ) b
         ";
-        $result = DB::select($sql);dd($sql);
+        $result = DB::select($sql);dd($sql);exit;
 
         return response()->json([
             'code'	=> 200,
