4abdc07234c56c0ab5cab2227a93a51d840b8e83
ceduce <ceduce@gmail.com>
2023-11-10 16:43:54 +0900

    fix(ord03) - 사이즈 깨짐 처리

--- app/Http/Controllers/store/order/ord03Controller.php
+++ app/Http/Controllers/store/order/ord03Controller.php
@@ -1019,12 +1019,7 @@ class ord03Controller extends Controller
 			select 
 				o.goods_no, g.goods_nm, g.goods_nm_eng, g.style_no, o.goods_opt
 				, pc.prd_cd, concat(pc.brand, pc.year, pc.season, pc.gender, pc.item, pc.seq, pc.opt) as prd_cd_p, pc.color
-				, ifnull((
-					select s.size_cd from size s
-					where s.size_kind_cd = pc.size_kind
-					   and s.size_cd = pc.size
-					   and use_yn = 'Y'
-				),'') as size
+				, pc.size
 				, sum(o.qty) as order_qty, count(o.ord_opt_no) as order_cnt
 				, g.com_nm, b.brand_nm as brand
 				$qty_sql
@@ -1254,12 +1249,7 @@ class ord03Controller extends Controller
 					, g.goods_nm_eng
 					, concat(pc.brand, pc.year, pc.season, pc.gender, pc.item, pc.seq, pc.opt) as prd_cd_p
 					, pc.color
-					, ifnull((
-						select s.size_cd from size s
-						where s.size_kind_cd = pc.size_kind
-						   and s.size_cd = pc.size
-						   and use_yn = 'Y'
-					),'') as size
+					, pc.size
 					, o.goods_opt
 					, o.qty as sale_qty
 					, 0 as dlv_qty -- 배송처 재고수량
@@ -1439,12 +1429,7 @@ class ord03Controller extends Controller
 					, if(rcp.dlv_location_type = 'STORAGE', (select storage_nm from storage where storage_cd = rcp.dlv_location_cd), (select code_val from code where code_kind_cd = 'ONLINE_ORDER_STORE' and code_id = rcp.dlv_location_cd)) as dlv_location_nm
 					, o.ord_no, o.ord_opt_no, o.goods_no, g.goods_nm, g.goods_nm_eng, g.style_no, o.goods_opt
 					, pc.prd_cd, concat(pc.brand, pc.year, pc.season, pc.gender, pc.item, pc.seq, pc.opt) as prd_cd_p, pc.color
-					, ifnull((
-						select s.size_cd from size s
-						where s.size_kind_cd = pc.size_kind
-						   and s.size_cd = pc.size
-						   and use_yn = 'Y'
-					),'') as size
+					, pc.size
 					, o.wonga, o.price, g.price as goods_price, g.goods_sh, o.qty, o.dlv_no
 					, o.pay_type, o.dlv_amt, o.point_amt, o.coupon_amt, o.dc_amt, o.recv_amt
 					, o.sale_place, o.store_cd, o.ord_state, o.clm_state, o.com_id, o.baesong_kind as dlv_baesong_kind, o.ord_date
