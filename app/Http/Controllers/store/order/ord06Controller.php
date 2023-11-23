6568bdefaa3252cd326e456309d305b68ee57717
ceduce <ceduce@gmail.com>
2023-11-23 10:46:14 +0900

    fix(ord06) - 온라인 판매 내역 수정

--- app/Http/Controllers/store/order/ord06Controller.php
+++ app/Http/Controllers/store/order/ord06Controller.php
@@ -426,7 +426,7 @@ class ord06Controller extends Controller
                     ifnull(if(st.amt_kind = 'per', round(o.price * st.sale_per / 100), st.sale_amt), 0) as sale_kind_amt,
                     round((1 - (o.price / g.goods_sh)) * 100) as sale_dc_rate
                 from order_opt_wonga w
-                    inner join order_opt o on o.ord_opt_no = w.ord_opt_no
+                    inner join order_opt o on o.ord_opt_no = w.ord_opt_no and o.sale_kind is not null
                     left outer join product_code pc on pc.prd_cd = o.prd_cd
                     inner join order_mst om on o.ord_no = om.ord_no
                     inner join goods g on o.goods_no = g.goods_no
@@ -493,7 +493,7 @@ class ord06Controller extends Controller
                     sum(o.recv_amt * if(w.ord_state > 30, -1, 1)) as total_recv_amt,
                     sum(o.dlv_amt * if(w.ord_state > 30, -1, 1)) as total_dlv_amt
                 from order_opt_wonga w
-                    inner join order_opt o on o.ord_opt_no = w.ord_opt_no
+                    inner join order_opt o on o.ord_opt_no = w.ord_opt_no and o.sale_kind is not null
                     left outer join product_code pc on pc.prd_cd = o.prd_cd
                     inner join order_mst om on o.ord_no = om.ord_no
                     inner join goods g on o.goods_no = g.goods_no
