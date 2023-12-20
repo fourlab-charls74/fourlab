831824f10213c7b5f83d3316c69d6998a6dc1f14
ceduce <ceduce@gmail.com>
2023-12-20 10:43:07 +0900

    fix(sal27) - 초기화 전 주문 데이터 오류

--- app/Http/Controllers/store/sale/sal27Controller.php
+++ app/Http/Controllers/store/sale/sal27Controller.php
@@ -137,6 +137,7 @@ class  sal27Controller extends Controller
 					from order_opt_wonga aa
 					inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 					where aa.ord_state_date >= '" . $from . "' and aa.ord_state_date <= '" . $to . "'
+						and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 						and aa.ord_state in (30,60,61)
 						and bb.prd_cd = pc.prd_cd
 				) as sale_qty_" . $from
@@ -187,6 +188,7 @@ class  sal27Controller extends Controller
 						inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 						where 1=1 
 							$date_where1
+							and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 							and aa.ord_state in (30,60,61)
 							and bb.prd_cd = pc.prd_cd
 					) as sale_qty
@@ -196,6 +198,7 @@ class  sal27Controller extends Controller
 						inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 						where 1=1 
 							$date_where1
+							and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 							and aa.ord_state in (30,60,61)
 							and bb.prd_cd = pc.prd_cd
 					) as sale_price
@@ -205,6 +208,7 @@ class  sal27Controller extends Controller
 						inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 						where 1=1 
 							$date_where1
+							and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 							and aa.ord_state in (30,60,61)
 							and bb.prd_cd = pc.prd_cd
 					) as sale_wonga
@@ -214,6 +218,7 @@ class  sal27Controller extends Controller
 						inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 						where 1=1
 							$date_where1
+							and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 							and aa.ord_state in (30,60,61)
 							and bb.prd_cd = pc.prd_cd
 					) as sale_recv_price
@@ -224,6 +229,7 @@ class  sal27Controller extends Controller
 						inner join order_opt bb on aa.ord_opt_no = bb.ord_opt_no and bb.ord_state = '30'
 						where 1=1 
 							$date_where6
+							and if( aa.ord_state_date <= '20231109', bb.sale_kind is not null, 1=1)	-- 피엘라벤 초기화 특성
 							and aa.ord_state in (30,60,61)
 							and bb.prd_cd = pc.prd_cd
 					) as term_sale_qty
