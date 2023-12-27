28e02984b28d1152e8880c3914a5e6bfb68731c4
bigcastle <bigcastle5407@naver.com>
2023-12-27 10:27:13 +0900

    fix(stk26):매장실사/LOSS관리 size 수정

--- app/Http/Controllers/store/stock/stk26Controller.php
+++ app/Http/Controllers/store/stock/stk26Controller.php
@@ -495,14 +495,10 @@ class stk26Controller extends Controller
                     , if(g.goods_no <> '0', g.style_no, p.style_no) as style_no
                     , if(g.goods_no <> '0', g.goods_nm, p.prd_nm) as goods_nm
                     , if(g.goods_no <> '0', g.goods_nm_eng, p.prd_nm) as goods_nm_eng
-                    , concat(pc.brand, pc.year, pc.season, pc.gender, pc.item, pc.seq, pc.opt) as prd_cd_p
+                    -- , concat(pc.brand, pc.year, pc.season, pc.gender, pc.item, pc.seq, pc.opt) as prd_cd_p
+                    , pc.prd_cd_p
                     , pc.color
-                    , (
-                        select s.size_cd from size s
-                        where s.size_kind_cd = if(pc.size_kind != '', pc.size_kind, if(pc.gender = 'M', 'PRD_CD_SIZE_MEN', if(pc.gender = 'W', 'PRD_CD_SIZE_WOMEN', 'PRD_CD_SIZE_UNISEX')))
-                            and s.size_cd = pc.size
-                            and use_yn = 'Y'
-                    ) as size
+                    , pc.size
                     , pc.goods_opt
                     , if(g.goods_no <> '0', g.goods_sh, p.tag_price) as goods_sh
                     , if(g.goods_no <> '0', g.price, p.price) as price
