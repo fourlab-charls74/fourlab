a70b93789d32fddf41e2ad99d3da7d367bf5e2b3
ceduce <ceduce@gmail.com>
2024-01-03 17:54:10 +0900

    fix(sal02) - 주문상태 오류 수정

--- app/Http/Controllers/store/sale/sal02Controller.php
+++ app/Http/Controllers/store/sale/sal02Controller.php
@@ -167,7 +167,7 @@ class sal02Controller extends Controller
     				  if (s.online_only_yn = 'Y', sum(case when o.sale_kind = 81 then (o.recv_amt * if(w.ord_state > 30, -1, 1)) else 0 end), sum(case when o.sale_kind <> 81 or o.sale_kind is null then (o.recv_amt * if(w.ord_state > 30, -1, 1)) else 0 end))  as offline,
 					  ${sum}
 				   from order_opt_wonga w
-					  inner join order_opt o on w.ord_opt_no = o.ord_opt_no
+					  inner join order_opt o on w.ord_opt_no = o.ord_opt_no and o.ord_state = '30'
 					  inner join store s on s.store_cd = o.store_cd
 					  inner join order_mst m on m.ord_no = o.ord_no
 					  inner join goods g on g.goods_no = o.goods_no and g.goods_sub = o.goods_sub
